f48488f65c60e949d949f4c4b84fa671c6dddb28
MrMosk <kiryumetaldragon@gmail.com>
2017-10-30 11:07:02 -0600

    cup class

---
+++ src/Cup.java
@@ -0,0 +1,18 @@
+public class Cup {
+
+    boolean dead = true;
+    String[] playerRolls = new String[6];
+
+
+    private String difficulty1 = "e";
+    private String difficulty2 = "m";
+    private String difficulty3 = "h";
+
+    private String points = "brains";
+    private String damage = "bullet";
+    private String goAgain = "runner";
+
+    private int riskOfDying = 10;
+    private int riskOfDying2 = 50;
+    private int riskOfDying3 = 80;
+}
