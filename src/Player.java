48f4d73aa7c0c99036f624216e9b0864dd1ac34e
Michael Zopff <michael.zopff@gmail.com>
2017-10-30 11:07:27 -0600

    Rewrote Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,15 +1,34 @@
 public class Player {
-
-    //Players and their names
-    public static String Player1 = "";
-    public static String Player2 = "";
-    //Players' Brains Eaten
-    public int P1BE = 0;
-    public int P2BE = 0;
-    //Players' Survivors Cornered
-    public int P1SC = 0;
-    public int P2SC = 0;
-    //Players' Shots Fired
-    public int P1SF = 0;
-    public int P2SF = 0;
+    private String name;
+    private int brainsEaten = 0;
+    private int survivorsCornered = 0;
+    private int shotsFired = 0;
+    public String getName() {
+        return name;
+    }
+    public void setName(String playerName, int playerNumber) {
+        if (playerName.equals("")) {
+            name = "Player " + playerNumber;
+        } else {
+            name = playerName;
+        }
+    }
+    public int getBrainsEaten() {
+        return brainsEaten;
+    }
+    public void addBrainsEaten(int number) {
+        brainsEaten += number;
+    }
+    public int getSurvivorsCornered() {
+        return survivorsCornered;
+    }
+    public void setSurvivorsCornered(int number) {
+        survivorsCornered = number;
+    }
+    public int getShotsFired() {
+        return shotsFired;
+    }
+    public void setShotsFired(int number) {
+        shotsFired = number;
+    }
 }
